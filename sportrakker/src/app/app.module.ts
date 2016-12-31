0a4f5b39b7476dc61fc19bdaf5bcedc64f58dedf
Andrew Casner <a.casner@comcast.net>
2016-12-30 22:27:33 -0700

    Added new component 'new-meet'

--- sportrakker/src/app/app.module.ts
+++ sportrakker/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { NewMeetComponent } from './new-meet/new-meet.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    NewMeetComponent
   ],
   imports: [
     BrowserModule,
