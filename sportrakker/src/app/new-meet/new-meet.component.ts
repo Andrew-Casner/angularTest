32e4bdb55c5d131cfbcd5fb2fedd2822298a6ec4
Andrew Casner <a.casner@comcast.net>
2017-01-02 21:56:04 -0700

    Got angular Validation to work

--- sportrakker/src/app/new-meet/new-meet.component.ts
+++ sportrakker/src/app/new-meet/new-meet.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-new-meet',
-  templateUrl: './new-meet.component.html',
-  styleUrls: ['./new-meet.component.css']
+	selector: 'app-new-meet',
+	templateUrl: './new-meet.component.html',
+	styleUrls: ['./new-meet.component.css']
 })
 export class NewMeetComponent implements OnInit {
+	newMeetForm : FormGroup;
+	constructor(fb: FormBuilder) {
+		this.newMeetForm = fb.group({
+			'meetName' : [null, Validators.required],
+			'meetLocation' : [null, Validators.required]
+			
+		})
+	}
+	submitForm(value: any):void{
+		console.log(value);
+	}
 
-  constructor() { }
+	ngOnInit() {
 
-  ngOnInit() {
-  }
+	}
 
 }
